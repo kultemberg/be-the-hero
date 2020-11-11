67fc9ef5f9375426b89ef640561e0a722696eeb5
kultemberg <kultemberg@gmail.com>
2020-11-10 23:08:54 -0400

    front-end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,11 @@
-//const { response } = require('express');
-const express = require ('express');
-const cors = require('cors'); 5.2K(gziipped: 2.1K)
+const express = require('express');
+const cors = require('cors'); 
 const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333);
